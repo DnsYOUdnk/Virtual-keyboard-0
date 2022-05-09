15d66569c958c17e3de80836b743144ed2e32824
Dinis <d.yudenka@yandex.by>
2022-05-09 20:58:18 +0300

    feat: create keyboard data

---
+++ src/components/createKeyboard.js
@@ -0,0 +1,5 @@
+// const keyboardApp = document.createElement('div');
+//       keyboardApp.setAttribute('id','app__keyboard');
+// document.body.append(keyboardApp)
+
+// export default keyboardApp;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import './style.css';
+// import './style.css';
+// import {keyboardApp} from './components/createKeyboard.js'
 
-console.log('hEllo');
+
+
+/* document.addEventListener('keydown', (event) => {
+    //event.code - имя клавиши  event.key - значение клавиши; {name: , valueEng: , valueRus: }
+    // console.log(event.code, event.key, event.keyCode);
+    // console.log(event.getModifierState("CapsLock"))
+}) */
