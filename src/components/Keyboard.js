b7fd4d5e8e75c718813fb8a64b4fe3fb0f439903
Dinis <d.yudenka@yandex.by>
2022-05-10 02:58:32 +0300

    refactor: change a separate function to a common object method

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,9 @@
 import KeyboardData from './KeyboardData.js';
-import { createKeysKeyboard } from '../Utils/createKeysKeyboard.js';
 
 function Keyboard() {
       const keyboardProperties = localStorage.getItem('keyboardProperties') ? JSON.parse(localStorage.getItem('keyboardProperties')) : {lang: 'En', capsOn: false};
+      let shiftOn = false;
+
       this.init = () => {
             this.create();
             this.addEventKeysKeyboard();
@@ -14,32 +15,54 @@ function Keyboard() {
             this.keyboardWripper = document.createElement('div');
             this.keyboardWripper.classList.add('keyboard__wrapper');
             
-            this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, keyboardProperties.lang, false, keyboardProperties.capsOn);
+            this.getKeys();
             
             keyboardApp.appendChild(this.keyboardWripper);
             document.body.append(keyboardApp);
       };
 
+      this.getKeys = () => {
+            let newKeyboardData = KeyboardData,
+                { lang, capsOn } = keyboardProperties,
+                keyboardKeys = '';
+
+            newKeyboardData.forEach(element => {
+                  let nameKey = Object.keys(element)[0],
+                  {key, shiftKey = key} = element[nameKey]['value' + lang];
+                  key = key.length === 1 && capsOn ? key.toUpperCase() : key
+                  keyboardKeys +=  `
+                                          <div class="${nameKey} ${nameKey === 'CapsLock' && capsOn ? 'active' : ''} keyboard__keys">
+                                                ${!shiftOn ? key : shiftKey}
+                                          </div>
+                                    `
+            });
+            this.keyboardWripper.innerHTML = keyboardKeys;
+      }
+
       this.addEventKeysKeyboard = () => {
             document.addEventListener('keydown', (event) => {
-                  if(event.getModifierState("CapsLock")) {
-                        this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, keyboardProperties.lang, false, true);
-                  } else {
-                        this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, keyboardProperties.lang, false, false);
+                  switch(event.code) {
+                        case 'CapsLock' : keyboardProperties.capsOn =  !keyboardProperties.capsOn ? true : false;
+                        break;
+                        default :
+                        break;
                   }
-
-                  if(event.key === 'Shift') {
+                  
+                  this.getKeys()
+                  localStorage.setItem('keyboardProperties',JSON.stringify(keyboardProperties))
+                  /* if(event.key === 'Shift') {
                         // this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, 'En', true, false);
-                        document.addEventListener('keydown', (event) => {
-                              if(event.key === 'Alt'){
+                        document.addEventListener('keydown', (event1) => {
+                              if(event1.key === 'Alt'){
+                                    
                                     keyboardProperties.lang = keyboardProperties.lang === 'En' ? 'Ru' : 'En'
-                                    this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, keyboardProperties.lang, false, false);
+                                    this.keyboardWripper.innerHTML = this.getKeys(KeyboardData, keyboardProperties.lang, false, false);
                               }
                         })
                         // this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, 'En', false, true);
                   } else {
                         // this.keyboardWripper.innerHTML = createKeysKeyboard(KeyboardData, 'En', false, false);
-                  }
+                  } */
                   // event.code - имя клавиши  event.key - значение клавиши; {name: , valueEng: , valueRus: }
                   // console.log(event.code, event.key, event);
                   // console.log(event.getModifierState("CapsLock"))
